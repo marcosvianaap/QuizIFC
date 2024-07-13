ee54a066d9dd05e218bb67cc660074189904df0b
marcos <marcosap103@gmail.com>
2024-07-13 01:35:18 -0300

    update

--- app/models.py
+++ app/models.py
@@ -1,14 +1,71 @@
-# app/models.py
 from . import db
 
-class Curso(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(100), nullable=False)
-    turno = db.Column(db.String(50), nullable=False)
-    nivel = db.Column(db.String(50), nullable=False)
-    localidade = db.Column(db.String(100), nullable=False)
+class Dados_Gerais(db.Model):
+    __tablename__ = 'dados_gerais'
 
-class Caracteristica(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    curso_id = db.Column(db.Integer, db.ForeignKey('curso.id'), nullable=False)
-    descricao = db.Column(db.String(200), nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    instituicao = db.Column(db.String(255), nullable=False)
+    campus = db.Column(db.String(255), nullable=False)
+    cidade = db.Column(db.String(255), nullable=False)
+    estado = db.Column(db.String(255), nullable=False)
+    endereco = db.Column(db.String(255), nullable=False)
+    telefone = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable=False)
+    site = db.Column(db.String(255), nullable=False)
+    logo = db.Column(db.String(255), nullable=False)
+    instagram = db.Column(db.String(255), nullable=False)
+
+
+class Condicoes_Servicos(db.Model):
+    __tablename__ = 'condicoes_servicos'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    instituicao = db.Column(db.String(255), nullable=False)
+    campus = db.Column(db.String(255), nullable=False)
+    indice = db.Column(db.String(255), nullable=False)
+    opcao = db.Column(db.String(255), nullable=False)
+
+
+class Cursos_Ensino_Medio_Integrado(db.Model):
+    __tablename__ = 'cursos_ensino_medio_integrado'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    instituicao = db.Column(db.String(255), nullable=False)
+    campus = db.Column(db.String(255), nullable=False)
+    curso = db.Column(db.String(255), nullable=False)
+    pagina_do_curso = db.Column(db.String(255), nullable=False)
+    turno = db.Column(db.String(255), nullable=False)
+    duracao = db.Column(db.String(255), nullable=False)
+    areas_de_atuacao = db.Column(db.String(255), nullable=False)
+    forma_de_selecao = db.Column(db.String(255), nullable=False)
+    grade_curricular = db.Column(db.String(255), nullable=False)
+
+
+class Cursos_Subsequentes(db.Model):
+    __tablename__ = 'cursos_subsequentes'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    instituicao = db.Column(db.String(255), nullable=False)
+    campus = db.Column(db.String(255), nullable=False)
+    curso = db.Column(db.String(255), nullable=False)
+    pagina_do_curso = db.Column(db.String(255), nullable=False)
+    turno = db.Column(db.String(255), nullable=False)
+    duracao = db.Column(db.String(255), nullable=False)
+    areas_de_atuacao = db.Column(db.String(255), nullable=False)
+    forma_de_selecao = db.Column(db.String(255), nullable=False)
+    grade_curricular = db.Column(db.String(255), nullable=False)
+
+
+class Cursos_Graduacao(db.Model):
+    __tablename__ = 'cursos_graduacao'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    instituicao = db.Column(db.String(255), nullable=False)
+    campus = db.Column(db.String(255), nullable=False)
+    curso = db.Column(db.String(255), nullable=False)
+    pagina_do_curso = db.Column(db.String(255), nullable=False)
+    turno = db.Column(db.String(255), nullable=False)
+    duracao = db.Column(db.String(255), nullable=False)
+    areas_de_atuacao = db.Column(db.String(255), nullable=False)
+    forma_de_selecao = db.Column(db.String(255), nullable=False)
+    grade_curricular = db.Column(db.String(255), nullable=False)
