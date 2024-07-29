c518bb4acb9841e0d6ead51a21afc4d6b45ae67c
marcosvianaap <marcosap103@gmail.com>
2024-07-29 13:40:46 -0300

    update

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-# run
-from flask import Flask, render_template, redirect, request, flash
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key_here'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///IFC.db'
+app.secret_key = 'sua_chave_secreta'  
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/IFC.db'
 db = SQLAlchemy(app)
 
 class IFC(db.Model):
@@ -24,30 +23,54 @@ class IFC(db.Model):
 def home():
     return render_template('index.html')
 
-@app.route('/situacao')
-def tela_dois():
-    # Consulta ao banco de dados para buscar os dados que deseja exibir na tela dois
-    cursos = IFC.query.all()
-    
-    # Renderiza o template da tela dois com os dados dos cursos
-    return render_template('tela2.html', cursos=cursos)
+@app.route('/situacao', methods=['GET', 'POST'])
+def situacao():
+    if request.method == 'POST':
+        situacao = request.form.get('situacao')
+        session['situacao'] = situacao
+        return redirect(url_for('tela_tres'))
+    return render_template('tela2.html')
 
 @app.route('/regiao', methods=['GET', 'POST'])
 def tela_tres():
+    if request.method == 'POST':
+        regiao = request.form.get('regiao')
+        session['regiao'] = regiao
+        return redirect(url_for('tela_quatro'))
     return render_template('tela3.html')
 
 @app.route('/area', methods=['GET', 'POST'])
 def tela_quatro():
+    if request.method == 'POST':
+        area = request.form.get('area')
+        session['area'] = area
+        return redirect(url_for('resultado'))
     return render_template('tela4.html')
 
 @app.route('/ac', methods=['GET', 'POST'])
 def resultado():
+    if request.method == 'POST':
+        acao_afirmativa = request.form.get('acao_afirmativa')
+        session['acao_afirmativa'] = acao_afirmativa
+        return redirect(url_for('tela_cinco'))
     return render_template('tela5.html')
 
-@app.route('/resultado', methods=['GET', 'POST'])
+@app.route('/resultado', methods=['GET'])
 def tela_cinco():
-    return render_template('tela6.html')
+    # Recupera todas as respostas armazenadas na sessão
+    situacao = session.get('situacao')
+    regiao = session.get('regiao')
+    area = session.get('area')
+    acao_afirmativa = session.get('acao_afirmativa')
+
+    # Filtra os cursos recomendados com base nas escolhas do usuário
+    cursos_recomendados = IFC.query.filter_by(
+        Região=regiao,
+        Área_Temática=area,
+        Ação_Afirmativa=acao_afirmativa
+    ).all()
+
+    return render_template('tela6.html', cursos=cursos_recomendados)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
