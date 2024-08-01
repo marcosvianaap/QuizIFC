4666e9df5fbe49099634905d249d3b9dd3a7ffec
marcosvianaap <marcosap103@gmail.com>
2024-08-01 14:55:09 -0300

    Update2

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, redirect, url_for, request, session
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = 'quizifc'  
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/IFC.db'
+app.secret_key = os.getenv("SENHA") 
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(BASE_DIR, "instance", "IFC.db")}'
 db = SQLAlchemy(app)
 
 class IFC(db.Model):
@@ -27,14 +32,16 @@ def home():
 def tela_dois():
     if request.method == 'POST':
         situacao = request.form.get('situacao')
-        session['situacao'] = situacao
+        opcoes_selecionadas = situacao.split(', ')  # Armazena a lista de opções
+        session['situacao'] = opcoes_selecionadas  # Armazena a lista na sessão
         return redirect(url_for('tela_tres'))
     return render_template('tela2.html')
+
 
 @app.route('/regiao', methods=['GET', 'POST'])
 def tela_tres():
     if request.method == 'POST':
-        regiao = request.form.get('regiao')
+        regiao = request.form.get('regiao')   
         session['regiao'] = regiao
         return redirect(url_for('tela_quatro'))
     return render_template('tela3.html')
@@ -43,7 +50,8 @@ def tela_tres():
 def tela_quatro():
     if request.method == 'POST':
         area = request.form.get('area')
-        session['area'] = area
+        opcoes_selecionadas = area.split(', ')
+        session['area'] = opcoes_selecionadas
         return redirect(url_for('tela_cinco'))
     return render_template('tela4.html')
 
@@ -62,9 +70,14 @@ def result():
     regiao = session.get('regiao')
     area = session.get('area')
     acao_afirmativa = session.get('acao_afirmativa')
+    print(situacao,"\n" , regiao,"\n" , area, acao_afirmativa, "\n")
 
     # Inicia a consulta para cursos recomendados
     cursos_recomendados_query = IFC.query
+    
+     # Aplica filtro por situação, se especificado
+    if situacao:
+        cursos_recomendados_query = cursos_recomendados_query.filter(IFC.Modalidade.in_(situacao))
 
     # Aplica filtro por região, se especificado e não for "Em qualquer região"
     if regiao and regiao != "Em qualquer região":
@@ -72,7 +85,7 @@ def result():
 
     # Aplica filtro por área temática, exceto se for "Todas as áreas"
     if area and area != "Todas as áreas":
-        cursos_recomendados_query = cursos_recomendados_query.filter_by(Área_Temática=area)
+        cursos_recomendados_query = cursos_recomendados_query.filter(IFC.Área_Temática.in_(area))
 
       # Aplica filtro por ação afirmativa, se especificado e não for "Não, não pretendo"
     if acao_afirmativa and acao_afirmativa != "Não, não pretendo":
@@ -83,5 +96,16 @@ def result():
 
     return render_template('tela6.html', cursos=cursos_recomendados)
 
+
+@app.route('/reiniciar')
+def reiniciar_quiz():
+    session.clear()  # Limpa todos os dados da sessão
+    return redirect(url_for('home'))  # Redireciona para a página inicial do quiz
+
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
     app.run(debug=True)
+    
+  
